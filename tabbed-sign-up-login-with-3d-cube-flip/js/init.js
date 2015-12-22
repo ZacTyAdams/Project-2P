1ff81f94b3b0df84c134a7e9f62c5af964862415
ZacAdams <zta104@gmail.com>
2015-12-22 16:43:12 -0600

    Testing for new user profile object

--- tabbed-sign-up-login-with-3d-cube-flip/js/init.js
+++ tabbed-sign-up-login-with-3d-cube-flip/js/init.js
@@ -1,3 +1,30 @@
+var userProfile;
+
+function signUp(){
+	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
+
+	var user = new Parse.User();
+	var firstname = document.getElementById("fname").value;
+	var lastname = document.getElementById("lname").value;
+	var username = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	userProfile = {uname: username, fname: firstname, lname: lastname,};
+	
+	user.set("username", username);
+	user.set("password", password);
+	user.set("userprofile", userProfile);
+	
+	user.signUp(null, {
+		success: function(user){
+			console.log("success");
+		},
+		error: function(user, error){
+			console.log("Parse error: " + error.code + " " + error.message);
+		}
+	});
+}
+
 function Login(){
 	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
 
@@ -15,59 +42,28 @@ function Login(){
 
 }
 
-function signUp(){
-	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
-
-	var user = new Parse.User();
-	var firstname = document.getElementById("fname").value;
-	var lastname = document.getElementById("lname").value;
-	var username = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
-
-	user.set("username", username);
-	user.set("password", password);
-	user.set("firstname",firstname);
-	user.set("lastname", lastname);
-	
-	user.signUp(null, {
-		success: function(user){
-			console.log("success");
-		},
-		error: function(user, error){
-			console.log("Parse error: " + error.code + " " + error.message);
-		}
-	});
-}
-
-function tester(){
-	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
-
-	console.log(Parse.User.current().get("firstname"));
-}
-
 function loader(){ //pulling saved user data into userpane
 	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
-	
-	console.log("inside");
-	document.getElementById("username").innerHTML = "Hello " + Parse.User.current().get("firstname") + " " + Parse.User.current().get("lastname");
-	if(Parse.User.current().get("bio") != undefined){
-		document.getElementById("userbio").innerHTML = Parse.User.current().get("bio");
+	userProfile = Parse.User.current().get("userprofile");
+	//console.log(userProfile);
+	//checkforMatched();
+	//console.log("inside");
+	document.getElementById("username").innerHTML = "Hello " + userProfile.fname + " " + userProfile.lname;
+	if(userProfile.bio != undefined){
+		document.getElementById("userbio").innerHTML = userProfile.bio;
 	}
-	if(Parse.User.current().get("profilePic") != undefined){
-		document.getElementById('blah').src = Parse.User.current().get("profilePic"); 
-		console.log("pulling pic");
+	if(userProfile.pic != undefined){
+		document.getElementById('blah').src = userProfile.pic; 
 	}
-	if(Parse.User.current().get("tags") != undefined){
-		document.getElementById("tagfield").innerHTML = Parse.User.current().get("tags");
+	if(userProfile.tags != undefined){
+		document.getElementById("tagfield").innerHTML = userProfile.tags;
 	}
-	if(Parse.User.current().get("matched") != undefined){
-		var temp = Parse.User.current().get("matched").split(" ");
-		console.log(temp);
-		for(var i = 0; i<temp.length; i++){
+	if(userProfile.matched != undefined){
+		for(var i = 0; i<userProfile.matched.length; i++){
 			var matched = document.createElement("LI");
 			var matchedname = document.createElement("DIV");
 			var matchednumber = document.createElement("DIV");
-			var t = document.createTextNode(temp[i]);
+			var t = document.createTextNode(userProfile.matched[i].uname);
 			var q = document.createTextNode(i+1);
 			matchedname.appendChild(t);
 			matchednumber.appendChild(q);
@@ -76,24 +72,27 @@ function loader(){ //pulling saved user data into userpane
 			document.getElementById("matchedList").appendChild(matched);
 		}
 	}
-	matchFinder();
+	//matchFinder();
 
 }
 
 function save(){
 	Parse.initialize("aCJKTtGB1GBe7BigaQuTiim9H7o3Gg6NAQrFu9xH", "B7ZpMdjyn7EhwyqGqovr7Wr2KzV2HtlhG7Te5YjJ");
 	
-	console.log(document.getElementById("blah").src);
+	userProfile.bio = document.getElementById("userbio").innerHTML;
+	userProfile.tags = document.getElementById("tagfield").innerHTML.split(",");
+	//console.log(document.getElementById("blah").src);
 	if(document.getElementById("file-input").files[0] == null){
-			Parse.User.current().set("profilePic", Parse.User.current().get("profilePic"));
-			Parse.User.current().set("bio", document.getElementById("userbio").innerHTML);
-			Parse.User.current().set("firstname", Parse.User.current().get("firstname"));
-			Parse.User.current().set("lastname", Parse.User.current().get("lastname"));
-			Parse.User.current().set("username", Parse.User.current().get("username"));
-			Parse.User.current().set("tags", document.getElementById("tagfield").innerHTML);
+			//Parse.User.current().set("profilePic", Parse.User.current().get("profilePic"));
+			//Parse.User.current().set("bio", document.getElementById("userbio").innerHTML);
+			//Parse.User.current().set("firstname", Parse.User.current().get("firstname"));
+			//Parse.User.current().set("lastname", Parse.User.current().get("lastname"));
+			//Parse.User.current().set("username", Parse.User.current().get("username"));
+			//Parse.User.current().set("tags", document.getElementById("tagfield").innerHTML);
+			Parse.User.current().set("userprofile", userProfile);
 			Parse.User.current().save(null, {
 				success: function(user){
-					console.log(Parse.User.current().get("username"));
+					console.log(userProfile);
 
 				},
 				error: function(error) {
@@ -104,15 +103,16 @@ function save(){
 	else{
 		var parseFile = new Parse.File("thing", document.getElementById("file-input").files[0]);
 		parseFile.save().then(function(parseFile){
-			var url = parseFile.url();
-			Parse.User.current().set("profilePic", url);
-			Parse.User.current().set("bio", document.getElementById("userbio").innerHTML);
-			Parse.User.current().set("firstname", Parse.User.current().get("firstname"));
-			Parse.User.current().set("lastname", Parse.User.current().get("lastname"));
-			Parse.User.current().set("username", Parse.User.current().get("username"));
+			userProfile.pic = parseFile.url();
+			//Parse.User.current().set("profilePic", url);
+			//Parse.User.current().set("bio", document.getElementById("userbio").innerHTML);
+			//Parse.User.current().set("firstname", Parse.User.current().get("firstname"));
+			//Parse.User.current().set("lastname", Parse.User.current().get("lastname"));
+			//Parse.User.current().set("username", Parse.User.current().get("username"));
+			Parse.User.current().set("userprofile", userProfile);
 			Parse.User.current().save(null, {
 				success: function(user){
-					console.log(Parse.User.current().get("username"));
+					console.log(userProfile);
 
 				},
 				error: function(error) {
@@ -186,8 +186,8 @@ function showMatches(){
 }
 
 function likePerson(ele){
-	if(Parse.User.current().get("liked") == undefined){
-		Parse.User.current().set("liked", array[ele].name + " ");
+	if(Parse.User.current().get("liked") == undefined || Parse.User.current().get("liked") == ""){
+		Parse.User.current().set("liked", array[ele].name);
 		save();
 		console.log("in the conditional");
 	}
@@ -201,7 +201,7 @@ function likePerson(ele){
 			}
 		}
 		if(!isPresent){
-			Parse.User.current().set("liked", Parse.User.current().get("liked") + array[ele].name + " ");
+			Parse.User.current().set("liked", Parse.User.current().get("liked") + " " + array[ele].name);
 			save();
 		}
 	}
@@ -209,7 +209,7 @@ function likePerson(ele){
 	console.log(array[ele].name);
 }
 
-function sandbox(){
+function checkforMatched(){
 	var matches = Parse.User.current().get("liked").split(" ");
 	var query = new Parse.Query(Parse.User);
 	query.contains("liked", Parse.User.current().get("username"));
@@ -220,8 +220,8 @@ function sandbox(){
 					if(result[i].get("username") == matches[x]){
 						console.log("Match found: " + matches[x]);
 						Parse.User.current().set("liked", Parse.User.current().get("liked"));
-						if(Parse.User.current().get("matched") == undefined){
-							Parse.User.current().set("matched", matches[x] + " ");
+						if(Parse.User.current().get("matched") == undefined || Parse.User.current().get("matched") == ""){
+							Parse.User.current().set("matched", matches[x]);
 							save();
 						}
 						else{
@@ -233,7 +233,7 @@ function sandbox(){
 								}
 							}
 							if(!isPresent){
-								Parse.User.current().set("matched", Parse.User.current().get("matched") + " ");
+								Parse.User.current().set("matched", Parse.User.current().get("matched"));
 								save();
 							}
 						}
@@ -249,6 +249,72 @@ function sandbox(){
 	//for(var i = 0; i<matches.length; i++){
 	//	console.log(matches[i]);
 	//}
+}
+
+function sandbox(){
+	//array =[];
+	//var tags = Parse.User.current().get("tags").split(" ");
+	var query = new Parse.Query(Parse.User);
+	var matches = [];
+
+	//for(var i = 0; i<tags.length; i++)
+	//{
+		//console.log(tags[i]);
+		query.exists("userprofile");
+		query.find({
+			success: function(result){
+				var possiblematch = [];
+				for(x in result){
+					if(result[x].get("userprofile").uname != userProfile.uname){
+						var alreadymatched = false;
+						var compare = userProfile.tags.concat(result[x].get("userprofile").tags).sort();
+						for(var y = 0; y<compare.length; y++){
+							//console.log(compare[y] + " " + compare[y+1]);
+							if(compare[y] == compare[y+1] && !alreadymatched){
+								temp = {prof: result[x].get("userprofile"), hits: 1};
+								alreadymatched = true;
+							}
+							else if(compare[y] == compare[y+1] && alreadymatched){
+								temp.hits++;
+							}
+						}
+						if(alreadymatched){
+							matches.push(temp);
+						}
+					}
+				}
+				//console.log(possiblematch);
+				
+				console.log(matches);
+
+
+				/*for(var x = 0; x<result.length; x++){
+					if(result[x].get("username") != Parse.User.current().get("username")){
+						//console.log(result[x].get("username"));
+						var isPresent = false;
+						for(var y = 0; y<array.length; y++){
+							//console.log("its in the for");
+							if(array[y].name == result[x].get("username")){
+								isPresent = true;
+								array[y].hits++; 
+							}
+						}
+						if(!isPresent){
+							//console.log("its in");
+							var match = {name: result[x].get("username"), hits: 1, pic: result[x].get("profilePic")};
+							//console.log(match.name);
+							array.push(match);
+						}
+					}
+				}*/
+			},
+			error: function(error){
+				console.log("nothingness");
+			}
+		});
+
+	//}
+
 }
 
 
