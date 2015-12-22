085c3522b4ede26fb5c91da3fc8354fd88df1065
Scott <ssmckeefer@sbcglobal.net>
2015-12-22 16:44:59 -0600

    location on settings

--- tabbed-sign-up-login-with-3d-cube-flip/js/UI.js
+++ tabbed-sign-up-login-with-3d-cube-flip/js/UI.js
@@ -81,3 +81,21 @@ function readURL(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+
+
+
+  function getLocation() {
+    var x = document.getElementById("demo");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  function showPosition(position) {
+    var x = document.getElementById("demo");
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;  
+  }
